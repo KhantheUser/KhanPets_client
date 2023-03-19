9429819e71e290cbb0e188e4dd1794add1cb466f
Thien Duc <ducthiennguyen1601@gmail.com>
2023-03-19 12:20:54 +0700

    19-3-4

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,7 +13,26 @@ function HomePage() {
     if (window.FB) {
       window.FB.XFBML.parse();
     }
-
+    let locale = "vi_VN";
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: process.env.REACT_APP_FACEBOOK_APP_ID,
+        cookie: true, // enable cookies to allow the server to access
+        // the session
+        xfbml: true, // parse social plugins on this page
+        version: "v2.5", // use version 2.1
+      });
+    };
+    // Load the SDK asynchronously
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = `//connect.facebook.net/${locale}/sdk.js`;
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
     window.ahachatCustomerPluginData = {
       fb_id: "116941284660054",
       bot_id: 69395117,
